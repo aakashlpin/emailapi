0d6063ede855098aab771d1d456f59c1de6d029d
Aakash Goel <aakash.lpin@gmail.com>
2020-09-09 14:33:34 +0530

    works locally but not on prod

--- pages/api/email-search/attachment-unlock.js
+++ pages/api/email-search/attachment-unlock.js
@@ -1,4 +1,8 @@
 import axios from 'axios';
+import puppeteer from 'puppeteer';
+import qs from 'qs';
+import fs from 'fs';
+
 import ensureAuth from '~/src/middleware/ensureAuth';
 import queues from '~/src/redis-queue';
 
@@ -9,6 +13,63 @@ const { fetchEmailByMessageId, processMessageBody } = require('~/src/gmail');
 const { getAfterTs } = require('~/src/apps/utils');
 
 require('~/src/queues');
+
+async function extractTablesInExcel(filePath, filename) {
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1366, height: 768 });
+  await page.goto('http://127.0.0.1:5000/files');
+  await page.waitForSelector('input[type=file]');
+  await page.waitFor(1000);
+
+  const inputUploadHandle = await page.$('input[type=file]');
+
+  const fileToUpload = filePath;
+
+  inputUploadHandle.uploadFile(fileToUpload);
+
+  await page.type('#pages', 'all');
+
+  await page.waitForSelector('#upload');
+  await page.evaluate(() => document.getElementById('upload').click());
+
+  await page.waitForSelector('.jumbotron.control-panel');
+  await page.waitFor(1000);
+
+  await page.click('.detect-areas');
+
+  await page.waitFor(1000);
+
+  await page.click(
+    'div.container div.row section.col-md-2 button.btn.btn-success',
+  );
+
+  // extracted data page
+  await page.waitForSelector('#format');
+
+  const urlParams = page.url().split('/');
+  const jobId = urlParams[urlParams.length - 1];
+  const response = await axios.post(
+    'http://127.0.0.1:5000/download',
+    qs.stringify({
+      format: 'Excel', // or 'Excel'
+      job_id: jobId,
+    }),
+    {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Accept: 'application/zip',
+      },
+      responseType: 'arraybuffer',
+    },
+  );
+
+  fs.writeFileSync(`${filename}.zip`, response.data);
+
+  await browser.close();
+}
 
 async function handle(req, res, resolve) {
   const {
@@ -41,16 +102,23 @@ async function handle(req, res, resolve) {
       const localFilePath = `/tmp/${filename}`;
       base64.base64Decode(data.base64, localFilePath);
 
+      const outputFilePath = `/tmp/${filename
+        .split('.')
+        .map((v, idx) => (idx === 0 ? `${v}_unlocked` : v))
+        .join('.')}`;
+
       const params = {
         inputFilePath: localFilePath,
         password: pdfPasswordInput,
-        outputFilePath: `/tmp/${filename
-          .split('.')
-          .map((v, idx) => (idx === 0 ? `${v}_unlocked` : v))
-          .join('.')}`,
+        outputFilePath,
       };
 
       removePdfPassword(params);
+
+      await extractTablesInExcel(outputFilePath, filename);
+
+      res.json({});
+      return resolve();
 
       const messageBody = await fetchEmailByMessageId({
         messageId,
