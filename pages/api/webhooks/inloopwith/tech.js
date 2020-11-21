1e41c960f89615cca276000ca4939de76a71bfad
Aakash Goel <aakash.lpin@gmail.com>
2020-11-21 17:45:26 +0530

    feat: added `image` to feed item and fixed date format

--- pages/api/webhooks/inloopwith/tech.js
+++ pages/api/webhooks/inloopwith/tech.js
@@ -1,27 +1,26 @@
 import axios from 'axios';
 import { Promise } from 'bluebird';
-import { format } from 'date-fns';
-// import queues from '~/src/redis-queue';
 import { genericErrorHandler } from '../../../../src/utils';
 
 const { BitlyClient, isBitlyErrResponse } = require('bitly');
 const jsdom = require('jsdom');
-// const getUrls = require('get-urls');
-
-const { BITLY_ACCESS_TOKEN, INLOOPWITH_API_KEY } = process.env;
-
-const bitly = new BitlyClient(BITLY_ACCESS_TOKEN);
+const getUrls = require('get-urls');
 
 const { JSDOM } = jsdom;
 
+const { BITLY_ACCESS_TOKEN, INLOOPWITH_API_KEY, INLOOPWITH_URL } = process.env;
+
+const bitly = new BitlyClient(BITLY_ACCESS_TOKEN);
+
 require('~/src/queues');
 
-const getHumanDate = (date) => format(new Date(date), 'dd MMM yy');
-
-//     // https://inloopwith.netlify.app/api/feed
-
-// 'https://webhook.site/cf44478c-5558-4aff-80cf-dd20aa42e84b';
-const INLOOPWITH_URL = 'https://inloopwith.netlify.app/api/webhooks/feed';
+const postToInLoopWith = async (body) => {
+  await axios.post(INLOOPWITH_URL, body, {
+    headers: {
+      'x-ilw-api-key': INLOOPWITH_API_KEY,
+    },
+  });
+};
 
 function mergeDataKeys(dataItem, keys) {
   const mergedData = [...new Array(dataItem[keys[0]].length)].map(() => ({}));
@@ -44,6 +43,63 @@ function mergeDataKeys(dataItem, keys) {
 
   return mergedData;
 }
+
+const urlImageIsAccessible = async (url) => {
+  const correctedUrls = getUrls(url);
+  if (correctedUrls.size !== 0) {
+    const urlResponse = await axios(correctedUrls.values().next().value);
+    const contentType = urlResponse.headers['content-type'];
+    return new RegExp('image/*').test(contentType);
+  }
+  return false;
+};
+
+const getImage = async (htmlString, uri) => {
+  try {
+    const dom = new JSDOM(htmlString);
+    const { document } = dom.window;
+
+    const ogImg = document.querySelector('meta[property="og:image"]');
+    if (
+      ogImg != null &&
+      ogImg.content.length > 0 &&
+      (await urlImageIsAccessible(ogImg.content))
+    ) {
+      return ogImg.content;
+    }
+    const imgRelLink = document.querySelector('link[rel="image_src"]');
+    if (
+      imgRelLink != null &&
+      imgRelLink.href.length > 0 &&
+      (await urlImageIsAccessible(imgRelLink.href))
+    ) {
+      return imgRelLink.href;
+    }
+    const twitterImg = document.querySelector('meta[name="twitter:image"]');
+    if (
+      twitterImg != null &&
+      twitterImg.content.length > 0 &&
+      (await urlImageIsAccessible(twitterImg.content))
+    ) {
+      return twitterImg.content;
+    }
+    const imgs = Array.from(document.getElementsByTagName('img'));
+    if (imgs.length > 0) {
+      // eslint-disable-next-line no-return-assign
+      imgs.forEach((img) =>
+        img.src.indexOf('//') === -1
+          ? // eslint-disable-next-line no-param-reassign
+            (img.src = `${new URL(uri).origin}/${img.src}`)
+          : img.src,
+      );
+      return imgs[0].src;
+    }
+
+    return null;
+  } catch (e) {
+    return null;
+  }
+};
 
 const getDescription = async (htmlString) => {
   try {
@@ -76,41 +132,45 @@ async function linkPreviewGenerator(url) {
     const source = axios.CancelToken.source();
     setTimeout(() => {
       source.cancel();
-    }, 60 * 1000);
+    }, 30 * 1000);
 
     const { data } = await axios({
       method: 'GET',
       url,
-      timeout: 60 * 1000,
+      timeout: 30 * 1000,
       cancelToken: source.token,
     });
 
     if (!data) {
       return null;
     }
+    const image = await getImage(data);
     const description = await getDescription(data);
     return {
+      socialImage: image,
       socialDescription: description,
     };
   } catch (e) {
-    console.log('linkPreviewGenerator error', e);
+    // console.log('linkPreviewGenerator error', e);
     return null;
   }
 }
 
 const generateShortLink = async (longUrl) => {
-  try {
-    const response = await bitly.shorten(longUrl);
-    return response.link;
-  } catch (error) {
-    if (isBitlyErrResponse(error)) {
-      // Inferred type by TS is `BitlyErrorResponse`
-      console.log(`Bitly error: ${error.description}`);
-    } else {
-      genericErrorHandler(error);
-    }
-    return null;
-  }
+  // [TODO] bring this back
+  return null;
+  // try {
+  //   const response = await bitly.shorten(longUrl);
+  //   return response.link;
+  // } catch (error) {
+  //   if (isBitlyErrResponse(error)) {
+  //     // Inferred type by TS is `BitlyErrorResponse`
+  //     console.log(`Bitly error: ${error.description}`);
+  //   } else {
+  //     genericErrorHandler(error);
+  //   }
+  //   return null;
+  // }
 };
 
 const getLinkPreviewData = async (array, linkPreviewKey, longUrlKeys) => {
@@ -120,7 +180,12 @@ const getLinkPreviewData = async (array, linkPreviewKey, longUrlKeys) => {
       ? originalLink.split('?')[0]
       : originalLink;
 
-    const previewData = await linkPreviewGenerator(article[linkPreviewKey]);
+    let previewData = null;
+    try {
+      previewData = await linkPreviewGenerator(article[linkPreviewKey]);
+    } catch (e) {
+      // console.log(e);
+    }
     const allShortLinks = await Promise.all(
       longUrlKeys.map((urlKey) =>
         generateShortLink(
@@ -151,169 +216,85 @@ const getLinkPreviewData = async (array, linkPreviewKey, longUrlKeys) => {
   return props.filter((item) => item);
 };
 
-// function sendWhatsApp(content) {
-//   queues.sendWhatsAppQueue.add({
-//     path: '/sendText',
-//     args: [WA_SELF_NUMBER, content],
-//   });
-// }
-
-const contentFooter = [
-  '_fin_',
-  `Feedback and ❤️ always appreciated! Have a great day ahead ✨`,
-].join('\n');
-
 async function getProductHuntDigest(dataItem, date) {
-  const humanDate = getHumanDate(date);
-  const keys = ['ph_product_name', 'ph_upvotes', 'ph_comments', 'ph_title'];
-  if (!keys.every((key) => dataItem[key])) {
-    console.log('sending getProductHuntDigest failed!');
-    return;
-  }
-
-  const productHunt = mergeDataKeys(dataItem, keys);
-  const props = await getLinkPreviewData(productHunt, 'ph_product_name_link', [
-    'ph_product_name_link',
-  ]);
-
-  const feedItems = props.map(
-    ({ previewData, shortLinksRef, originalData }) => ({
-      name: originalData.ph_product_name,
-      title: originalData.ph_title,
-      description: previewData?.socialDescription,
-      upvotes_count: originalData.ph_upvotes,
-      comments_count: originalData.ph_comments,
-      short_link: shortLinksRef.ph_product_name_link_shortlink,
-      original_link: originalData.ph_product_name_link,
-    }),
-  );
-
   try {
-    // fire and forget
+    const keys = ['ph_product_name', 'ph_upvotes', 'ph_comments', 'ph_title'];
+    if (!keys.every((key) => dataItem[key])) {
+      console.log('sending getProductHuntDigest failed!');
+      return;
+    }
+
+    const productHunt = mergeDataKeys(dataItem, keys);
+    const props = await getLinkPreviewData(
+      productHunt,
+      'ph_product_name_link',
+      ['ph_product_name_link'],
+    );
+
+    const feedItems = props.map(
+      ({ previewData, shortLinksRef, originalData }) => ({
+        name: originalData.ph_product_name,
+        title: originalData.ph_title,
+        description: previewData?.socialDescription,
+        image: previewData?.socialImage,
+        upvotes_count: originalData.ph_upvotes,
+        comments_count: originalData.ph_comments,
+        short_link: shortLinksRef.ph_product_name_link_shortlink,
+        original_link: originalData.ph_product_name_link,
+      }),
+    );
+
     const postBody = {
       tag: 'product_hunt',
-      feed_date: date,
+      feed_date: new Date(date).toISOString(),
       items: feedItems,
     };
 
-    axios.post(INLOOPWITH_URL, postBody, {
-      headers: {
-        'x-ilw-api-key': INLOOPWITH_API_KEY,
-      },
-    });
+    await postToInLoopWith(postBody);
   } catch (e) {
     console.log(e);
   }
-
-  const productHuntContentBody = props
-    .map(({ previewData, shortLinksRef, originalData }, idx) => {
-      return [
-        `${idx + 1}. *${originalData.ph_product_name}* — ${
-          originalData.ph_title
-        }`,
-        previewData?.socialDescription
-          ? `\n_"${previewData.socialDescription.trim()}"_`
-          : null,
-        `👍 ${originalData.ph_upvotes} | 💬 ${originalData.ph_comments} | ${
-          shortLinksRef.ph_product_name_link_shortlink ||
-          originalData.ph_product_name_link
-        }`,
-      ]
-        .filter((i) => i)
-        .join('\n');
-    })
-    .join('\n\n\n');
-
-  const productHuntContentHeader = [
-    `🦄 *Product Hunt Digest | ${humanDate}*`,
-  ].join('\n');
-
-  const productHuntPost = [
-    productHuntContentHeader,
-    productHuntContentBody,
-    contentFooter,
-  ].join('\n\n');
-
-  console.log({ productHuntPost });
-
-  // sendWhatsApp(productHuntPost);
 }
 
 async function getHackerNewsDigest(dataItem, date) {
-  const humanDate = getHumanDate(date);
-  const keys = ['hn_title', 'hn_upvotes', 'hn_comments', 'hn_source_website'];
-  if (!keys.every((key) => dataItem[key])) {
-    console.log('sending getHackerNewsDigest failed!');
-    return;
-  }
-
-  const hackerNews = mergeDataKeys(dataItem, keys);
-  const itemsWithVotesGreaterThan = (items, votes) =>
-    items.filter((item) => Number(item.hn_upvotes) >= votes);
-
-  const props = await getLinkPreviewData(
-    itemsWithVotesGreaterThan(hackerNews, 100),
-    'hn_title_link',
-    ['hn_title_link', 'hn_comments_link'],
-  );
-
-  const feedItems = props.map(
-    ({ previewData, shortLinksRef, originalData }) => ({
-      title: originalData.hn_title,
-      description: previewData?.socialDescription,
-      upvotes_count: originalData.hn_upvotes,
-      comments_count: originalData.hn_comments,
-      short_link: shortLinksRef.hn_comments_link_shortlink,
-      original_link: originalData.hn_comments_link,
-    }),
-  );
-
   try {
-    // fire and forget
+    const keys = ['hn_title', 'hn_upvotes', 'hn_comments', 'hn_source_website'];
+    if (!keys.every((key) => dataItem[key])) {
+      console.log('sending getHackerNewsDigest failed!');
+      return;
+    }
+
+    const hackerNews = mergeDataKeys(dataItem, keys);
+    const itemsWithVotesGreaterThan = (items, votes) =>
+      items.filter((item) => Number(item.hn_upvotes) >= votes);
+
+    const props = await getLinkPreviewData(
+      itemsWithVotesGreaterThan(hackerNews, 100),
+      'hn_title_link',
+      ['hn_title_link', 'hn_comments_link'],
+    );
+
+    const feedItems = props.map(
+      ({ previewData, shortLinksRef, originalData }) => ({
+        title: originalData.hn_title,
+        description: previewData?.socialDescription,
+        image: previewData?.socialImage,
+        upvotes_count: originalData.hn_upvotes,
+        comments_count: originalData.hn_comments,
+        short_link: shortLinksRef.hn_comments_link_shortlink,
+        original_link: originalData.hn_comments_link,
+      }),
+    );
+
     const postBody = {
       tag: 'hacker_news',
-      feed_date: date,
+      feed_date: new Date(date).toISOString(),
       items: feedItems,
     };
-    axios.post(INLOOPWITH_URL, postBody, {
-      headers: {
-        'x-ilw-api-key': INLOOPWITH_API_KEY,
-      },
-    });
+    await postToInLoopWith(postBody);
   } catch (e) {
     console.log(e);
   }
-
-  const hackerNewsContentBody = props
-    .map(({ previewData, shortLinksRef, originalData }, idx) => {
-      return [
-        `${idx + 1}. *${originalData.hn_title}*`,
-        previewData?.socialDescription
-          ? `\n_"${previewData.socialDescription.trim()}"_`
-          : null,
-        `👍 ${originalData.hn_upvotes} | 💬 ${originalData.hn_comments} | ${
-          shortLinksRef.hn_comments_link_shortlink ||
-          originalData.hn_comments_link
-        }`,
-      ]
-        .filter((i) => i)
-        .join('\n');
-    })
-    .filter((i) => i)
-    .join('\n\n\n');
-
-  const hackerNewsContentHeader = [
-    `⚡️ *Hacker News Digest | ${humanDate}*`,
-  ].join('\n');
-
-  const hackerNewsPost = [
-    hackerNewsContentHeader,
-    hackerNewsContentBody,
-    contentFooter,
-  ].join('\n\n');
-
-  console.log({ hackerNewsPost });
-  // sendWhatsApp(hackerNewsPost);
 }
 
 export default async function handle(req, res) {
