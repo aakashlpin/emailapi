919af198dfb52d369489fcee5f0ec2f808729ef1
Aakash Goel <aakash.lpin@gmail.com>
2021-01-26 18:11:11 +0530

    withplugins

--- next.config.js
+++ next.config.js
@@ -1,18 +1,17 @@
 const nextEnv = require('next-env');
+const withPlugins = require('next-compose-plugins');
 const withCSS = require('@zeit/next-css');
 const aliases = require('./alias-config');
 
-const withNextEnv = nextEnv();
+const nextConfig = {
+  webpack: (config) => {
+    const { alias } = config.resolve;
+    config.resolve.alias = { // eslint-disable-line
+      ...alias,
+      ...aliases,
+    };
+    return config;
+  },
+};
 
-module.exports = withNextEnv(
-  withCSS({
-    webpack: (config) => {
-      const { alias } = config.resolve;
-      config.resolve.alias = { // eslint-disable-line
-        ...alias,
-        ...aliases,
-      };
-      return config;
-    },
-  }),
-);
+module.exports = withPlugins([nextEnv(), withCSS()], nextConfig);
