6c43c25e7614d38a0606260671514a6659c6ff10
Aakash Goel <aakash.lpin@gmail.com>
2021-01-26 18:55:26 +0530

    added env for public keys

--- next.config.js
+++ next.config.js
@@ -3,7 +3,25 @@ const withPlugins = require('next-compose-plugins');
 const withCSS = require('@zeit/next-css');
 const aliases = require('./alias-config');
 
+const withNextEnv = nextEnv();
+
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY:
+      process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN:
+      process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    NEXT_PUBLIC_FIREBASE_DATABASE_URL:
+      process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    NEXT_PUBLIC_EMAILAPI_DOMAIN: process.env.NEXT_PUBLIC_EMAILAPI_DOMAIN,
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID:
+      process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    NEXT_PUBLIC_GOOGLE_OAUTH_REDIRECT_URI:
+      process.env.NEXT_PUBLIC_GOOGLE_OAUTH_REDIRECT_URI,
+    NEXT_PUBLIC_SENDING_EMAIL_ID: process.env.NEXT_PUBLIC_SENDING_EMAIL_ID,
+  },
+  serverRuntimeConfig: {},
   webpack: (config) => {
     const { alias } = config.resolve;
     config.resolve.alias = { // eslint-disable-line
@@ -14,4 +32,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withPlugins([nextEnv(), withCSS], nextConfig);
+module.exports = withPlugins([withNextEnv, withCSS], nextConfig);
